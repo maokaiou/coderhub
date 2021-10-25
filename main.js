a8e03ca1233b3a88af89eb10d8dc1a88c451f371
maokaiou <2827017101@qq.com>
2021-10-25 21:04:23 +0800

    目录结构搭建进一步完善

--- main.js
+++
@@ -1,6 +0,0 @@
-const Koa = require('koa');
-const app = new Koa();
-
-app.listen(8888,(ctx,next)=>{
-    console.log('项目启动成功')
-})
---
+++ src/main.js
@@ -0,0 +1,5 @@
+const app = require('./app')
+
+app.listen(8880,(ctx,next)=>{
+    console.log('项目启动成功')
+})
